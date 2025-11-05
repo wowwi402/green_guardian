e2ed6dad0523cb25777cdb8c2f7d0a4d47013fad
wowwi402 <wowwi422002@gmail.com>
2025-11-05 13:20:35 +0700

    Chua sua duoc loi dang nhap

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from './src/theme/ThemeProvider';
 import { AuthProvider } from './src/auth/AuthProvider';
 import RootNavigator from './src/navigation/RootNavigator';
+import { StatusBar } from 'expo-status-bar';
+
 
 export default function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <NavigationContainer>
-          <RootNavigator />
+        <RootNavigator />
+           <StatusBar style="dark" />
         </NavigationContainer>
       </AuthProvider>
     </ThemeProvider>
